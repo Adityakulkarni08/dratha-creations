75ecf41ae1280106953503a6208961baaaddca8d
Ranga <rangachimalgi123@gmail.com>
2024-02-28 20:37:43 +0530

    changes in intro for carousal

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -1,42 +1,70 @@
 import React from "react";
-import img from "../images/Web-developer.svg";
-import { Link } from "react-router-dom";
+import Slider from "react-slick";
 import { FaCameraRetro, FaBirthdayCake, FaGift } from 'react-icons/fa';
 import { IoFastFoodOutline } from 'react-icons/io5';
 import { MdOutlineRoomService } from 'react-icons/md';
-import "../styles/intro.css"
+import "../styles/intro.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Intro = () => {
+  const isMobile = window.innerWidth <= 768; // Adjust breakpoint as needed
+  const carouselSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6" id="about">
         <div
-          className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left"
+          className={`flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left ${isMobile ? 'mobile-carousel' : ''}`}
           data-aos="fade-up"
         >
-          <div className="icon-menu-container">
-            <div className="icon-menu-item">
-              <MdOutlineRoomService size={50} />
-              <p>SERVICES</p>
+          {isMobile ? (
+            <Slider {...carouselSettings}>
+              <div>
+              <div className="icon-menu-item">
+                <MdOutlineRoomService size={50} />
+                <p>SERVICES</p>
+              </div>
+              </div>
+              <div>
+              <div className="icon-menu-item">
+                <FaCameraRetro size={50} />
+                <p>PHOTOGRAPHY</p>
+              </div>
+              </div>
+              {/* Add more slides as needed */}
+            </Slider>
+          ) : (
+            <div className="icon-menu-container">
+              {/* Your existing icon menu items */}
+              <div className="icon-menu-item">
+                <MdOutlineRoomService size={50} />
+                <p>SERVICES</p>
+              </div>
+              <div className="icon-menu-item">
+                <FaCameraRetro size={50} />
+                <p>PHOTOGRAPHY</p>
+              </div>
+              <div className="icon-menu-item">
+                <FaBirthdayCake size={50} />
+                <p>CAKES</p>
+              </div>
+              <div className="icon-menu-item">
+                <FaGift size={50} />
+                <p>GIFTS</p>
+              </div>
+              <div className="icon-menu-item">
+                <IoFastFoodOutline size={50} />
+                <p>FOOD</p>
+              </div>
             </div>
-            <div className="icon-menu-item">
-              <FaCameraRetro size={50} />
-              <p>PHOTOGRAPHY</p>
-            </div>
-            <div className="icon-menu-item">
-              <FaBirthdayCake size={50} />
-              <p>CAKES</p>
-            </div>
-            {/* Repeat for each icon and category you have */}
-            <div className="icon-menu-item">
-              <FaGift size={50} />
-              <p>GIFTS</p>
-            </div>
-            <div className="icon-menu-item">
-              <IoFastFoodOutline size={50} />
-              <p>FOOD</p>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </>
