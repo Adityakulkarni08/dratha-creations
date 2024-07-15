9ac4f8ed02f25a7300edbe77faa18b97272f0c4b
Ranga <rangachimalgi123@gmail.com>
2024-07-15 21:07:21 +0530

    changes in the dropdown

--- client/src/pages/DropdownsComponent.js
+++ client/src/pages/DropdownsComponent.js
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../styles/dropdown.css";
 import newbornImage from "../images/birthday.jpg"; // Import the image
+import engagement from "../images/engagement.png";
 
 const DropdownComponent = () => {
   const [activeSection, setActiveSection] = useState(null);
+  const hideTimeout = useRef(null);
 
   const handleMouseEnter = (section) => {
+    clearTimeout(hideTimeout.current);
     setActiveSection(section);
   };
 
   const handleMouseLeave = () => {
-    setActiveSection(null);
+    hideTimeout.current = setTimeout(() => {
+      setActiveSection(null);
+    }, 300); // 300ms delay before hiding
   };
 
   return (
-    <div className="navbar" onMouseLeave={handleMouseLeave}>
-      <div className="dropdown">
+    <div className="navbar">
+      <div
+        className="dropdown"
+        onMouseEnter={handleMouseLeave}
+        onMouseLeave={handleMouseLeave}
+      >
         <button className="dropbtn">Decorations</button>
         <div className="dropdown-content">
           <div
@@ -24,7 +33,7 @@ const DropdownComponent = () => {
             onMouseLeave={handleMouseLeave}
           >
             Photography
-            {(activeSection === "Photography" || activeSection === null) && (
+            {activeSection === "Photography" && (
               <div
                 className="dropdown-subcontent"
                 onMouseEnter={() => handleMouseEnter("Photography")}
@@ -37,11 +46,7 @@ const DropdownComponent = () => {
                   <a href="#">Photo + Album</a>
                   <a href="#">Photo + Album + Video</a>
                 </div>
-                <img
-                  src={newbornImage}
-                  alt="Photography"
-                  className="dropdown-image"
-                />
+                <img src={newbornImage} alt="Photography" className="dropdown-image" />
               </div>
             )}
           </div>
@@ -51,7 +56,7 @@ const DropdownComponent = () => {
             onMouseLeave={handleMouseLeave}
           >
             Cakes
-            {(activeSection === "Cakes" || activeSection === null) && (
+            {activeSection === "Cakes" && (
               <div
                 className="dropdown-subcontent"
                 onMouseEnter={() => handleMouseEnter("Cakes")}
@@ -63,11 +68,7 @@ const DropdownComponent = () => {
                   <a href="#">Wedding Cakes</a>
                   <a href="#">Custom Cakes</a>
                 </div>
-                <img
-                  src={newbornImage}
-                  alt="Cakes"
-                  className="dropdown-image"
-                />
+                <img src={newbornImage} alt="Cakes" className="dropdown-image" />
               </div>
             )}
           </div>
@@ -77,7 +78,7 @@ const DropdownComponent = () => {
             onMouseLeave={handleMouseLeave}
           >
             Entertainment
-            {(activeSection === "Entertainment" || activeSection === null) && (
+            {activeSection === "Entertainment" && (
               <div
                 className="dropdown-subcontent"
                 onMouseEnter={() => handleMouseEnter("Entertainment")}
@@ -89,11 +90,7 @@ const DropdownComponent = () => {
                   <a href="#">Clowns</a>
                   <a href="#">Live Bands</a>
                 </div>
-                <img
-                  src={newbornImage}
-                  alt="Entertainment"
-                  className="dropdown-image"
-                />
+                <img src={newbornImage} alt="Entertainment" className="dropdown-image" />
               </div>
             )}
           </div>
@@ -103,7 +100,7 @@ const DropdownComponent = () => {
             onMouseLeave={handleMouseLeave}
           >
             Return Gifts
-            {(activeSection === "Return Gifts" || activeSection === null) && (
+            {activeSection === "Return Gifts" && (
               <div
                 className="dropdown-subcontent"
                 onMouseEnter={() => handleMouseEnter("Return Gifts")}
@@ -115,11 +112,7 @@ const DropdownComponent = () => {
                   <a href="#">Books</a>
                   <a href="#">Gift Cards</a>
                 </div>
-                <img
-                  src={newbornImage}
-                  alt="Return Gifts"
-                  className="dropdown-image"
-                />
+                <img src={newbornImage} alt="Return Gifts" className="dropdown-image" />
               </div>
             )}
           </div>
@@ -129,7 +122,7 @@ const DropdownComponent = () => {
             onMouseLeave={handleMouseLeave}
           >
             Food
-            {(activeSection === "Food" || activeSection === null) && (
+            {activeSection === "Food" && (
               <div
                 className="dropdown-subcontent"
                 onMouseEnter={() => handleMouseEnter("Food")}
@@ -145,6 +138,28 @@ const DropdownComponent = () => {
                 <img src={newbornImage} alt="Food" className="dropdown-image" />
               </div>
             )}
+          </div>
+        </div>
+      </div>
+      <div className="dropdown right-end">
+        <button className="dropbtn">
+          <span>Packages</span>
+        </button>
+        <div className="dropdown-content">
+          <div className="dropdown-item">
+            <img src={engagement} alt="Birthday Packages" />
+            <h3>Birthday Packages</h3>
+            <p>Curated event packages from the best event planners.</p>
+          </div>
+          <div className="dropdown-item">
+            <img src={engagement} alt="Housewarming Packages" />
+            <h3>Housewarming Packages</h3>
+            <p>Curated event packages from the best event planners.</p>
+          </div>
+          <div className="dropdown-item">
+            <img src={engagement} alt="Wedding Packages" />
+            <h3>Wedding Packages</h3>
+            <p>Curated event packages from the best event planners.</p>
           </div>
         </div>
       </div>
