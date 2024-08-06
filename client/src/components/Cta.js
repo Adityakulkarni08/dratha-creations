00d55716a9284800d9e113af812ba25cf4d8fab2
Ranga <rangachimalgi123@gmail.com>
2024-08-06 08:14:13 +0530

    CTA (Footer) design changes

--- client/src/components/Cta.js
+++ client/src/components/Cta.js
@@ -1,24 +1,258 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const categories = [
+    {
+        title: "New Born",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Photography",
+                subitems: ["Traditional Photography", "Candid Photography"],
+            },
+            {
+                heading: "Food",
+                subitems: [],
+            },
+        ],
+    },
+    {
+        title: "Baby Shower",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Decorations",
+                subitems: ["Decorations", "Garlands"],
+            },
+            {
+                heading: "Photography",
+                subitems: ["Traditional Photography", "Candid Photography"],
+            },
+            {
+                heading: "Makeup",
+                subitems: ["Traditional Makeup", "HD Makeup"],
+            },
+            {
+                heading: "Mehndi",
+                subitems: ["Traditional Designs", "Rajasthani Designs", "Other Designs"],
+            },
+            {
+                heading: "Entertainment",
+                subitems: [],
+            },
+            {
+                heading: "Return Gifts",
+                subitems: ["Traditional Gifts", "Fancy Gifts"],
+            },
+            {
+                heading: "Food",
+                subitems: [],
+            },
+        ],
+    },
+    {
+        title: "Birthday",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Decorations",
+                subitems: ["Balloon Decorations", "Theme Decoration"],
+            },
+            {
+                heading: "Photography",
+                subitems: ["Traditional Photography", "Candid Photography"],
+            },
+            {
+                heading: "Cakes",
+                subitems: ["Theme Birthday Cakes", "Assorted Birthday Cakes"],
+            },
+            {
+                heading: "Entertainment",
+                subitems: ["Indoor", "Outdoor", "Baby Entry"],
+            },
+            {
+                heading: "Return Gifts",
+                subitems: ["Traditional Gifts", "Fancy Gifts"],
+            },
+            {
+                heading: "Food",
+                subitems: ["Vegetarian", "Non-Vegetarian"],
+            },
+        ],
+    },
+    {
+        title: "House Warming",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Decorations",
+                subitems: ["Decorations", "Garlands"],
+            },
+            {
+                heading: "Photography",
+                subitems: ["Traditional Photography", "Candid Photography"],
+            },
+            {
+                heading: "Entertainment",
+                subitems: [],
+            },
+            {
+                heading: "Return Gifts",
+                subitems: ["Traditional Gifts", "Fancy Gifts"],
+            },
+            {
+                heading: "Food",
+                subitems: ["Vegetarian", "Non-Vegetarian"],
+            },
+        ],
+    },
+    {
+        title: "Engagement",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Decorations",
+                subitems: ["Decorations", "Garlands"],
+            },
+            {
+                heading: "Photography",
+                subitems: ["Traditional Photography", "Candid Photography"],
+            },
+            {
+                heading: "Makeup",
+                subitems: ["Traditional Makeup", "HD Makeup"],
+            },
+            {
+                heading: "Entertainment",
+                subitems: ["Indoor", "Outdoor"],
+            },
+            {
+                heading: "Return Gifts",
+                subitems: ["Traditional Gifts", "Fancy Gifts"],
+            },
+            {
+                heading: "Food",
+                subitems: [],
+            },
+        ],
+    },
+    {
+        title: "Wedding",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Decorations",
+                subitems: ["Decorations", "Garlands"],
+            },
+            {
+                heading: "Photography",
+                subitems: ["Traditional Photography", "Candid Photography"],
+            },
+            {
+                heading: "Cakes",
+                subitems: ["Wedding Cakes"],
+            },
+            {
+                heading: "Makeup",
+                subitems: ["Traditional Makeup", "HD Makeup"],
+            },
+            {
+                heading: "Mehndi",
+                subitems: ["Bridal Mehndi", "Traditional Designs", "Rajasthani Designs"],
+            },
+            {
+                heading: "Entertainment",
+                subitems: ["Indoor", "Outdoor"],
+            },
+            {
+                heading: "Return Gifts",
+                subitems: ["Traditional Gifts", "Fancy Gifts"],
+            },
+            {
+                heading: "Food",
+                subitems: [],
+            },
+        ],
+    },
+    {
+        title: "Puberty",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Decorations",
+                subitems: ["Decorations", "Garlands"],
+            },
+            {
+                heading: "Photography",
+                subitems: ["Traditional Photography", "Candid Photography"],
+            },
+            {
+                heading: "Makeup",
+                subitems: ["Traditional Makeup", "HD Makeup"],
+            },
+            {
+                heading: "Mehndi",
+                subitems: ["Traditional Designs", "Rajasthani Designs", "Other Designs"],
+            },
+            {
+                heading: "Entertainment",
+                subitems: ["Indoor", "Outdoor"],
+            },
+            {
+                heading: "Return Gifts",
+                subitems: ["Traditional Gifts", "Fancy Gifts"],
+            },
+            {
+                heading: "Food",
+                subitems: [],
+            },
+        ],
+    },
+    {
+        title: "Surprise Party",
+        color: "text-pink-600",
+        items: [
+            {
+                heading: "Decorations",
+                subitems: ["Balloon Decorations"],
+            },
+            {
+                heading: "Food",
+                subitems: [],
+            },
+        ],
+    },
+];
+
 const Cta = () => {
-    return ( 
-        <div className="w-full flex items-center justify-center text-white cta">
-            <div className="mx-8 w-full h-96 text-center lg:text-left py-16 px-12 flex lg:justify-between items-center">                    
-                <div className="w-full flex flex-col lg:flex-row lg:justify-around">
-                    <div className="mb-4">
-                        <p className='text-2xl md:text-4xl font-bold mb-4'>Are you ready to scale your business?</p>
-                        <p className="text-lg md:text-2xl">Get in touch and let us build something amazing <span className='font-black'>together!</span></p>
-                    </div>
-                    
-                    <div className="w-full lg:w-72 pt-6 lg:mx-12">
-                        <Link to="/contact" className="bg-transparent border hover:bg-blue-900 hover:border-blue-800 text-white justify-center text-center rounded-lg px-10 py-3 flex items-center group">Send a message
-                        <svg className="w-5 h-5 ml-1 group-hover:translate-x-2 duration-500 ease-in" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </Link>
-                    </div>
+    return (
+        <div className="w-full text-black bg-white py-16">
+            <div className="mx-8 w-full text-center">
+                <p className='text-2xl md:text-4xl font-bold mb-8'>More From The Home Events</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                    {categories.map((category, index) => (
+                        <div key={index} className="text-left">
+                            <h3 className={`text-xl font-bold mb-2 ${category.color}`}>{category.title}</h3>
+                            {category.items.map((item, idx) => (
+                                <div key={idx} className="mb-4">
+                                    <h4 className="font-bold">{item.heading}</h4>
+                                    <ul className="pl-4 list-disc">
+                                        {item.subitems.map((subitem, subIdx) => (
+                                            <li key={subIdx} className="ml-2">
+                                                <Link to={`/${subitem.replace(/\s+/g, '-').toLowerCase()}`} className="text-black hover:underline">
+                                                    {subitem}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Cta;
