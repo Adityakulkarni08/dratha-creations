b35c6b8c6f242419256ccd4fbc9a2ad60be222f8
Ranga <rangachimalgi123@gmail.com>
2024-08-10 10:05:31 +0530

    get started buttin

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import NavBar from "../components/Navbar/NavBar";
-import heroImg from "../images/web-dev.svg";
+import "../styles/hero.css";
 import banner from "../images/banner.png";
 import birthdayBanner from "../images/birthdaybanner.png";
-import houseWarming from "../images/housewarmingbanner.png"
-import yourCeleb from "../images/yourcelebbanner.png"
-import "../styles/hero.css";
+import houseWarming from "../images/housewarmingbanner.png";
+import yourCeleb from "../images/yourcelebbanner.png";
+import EnquiryForm from './EnquiryForm'; // Import your existing EnquiryForm component
 
 const images = [banner, birthdayBanner, houseWarming, yourCeleb];
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isFormOpen, setIsFormOpen] = useState(false); // State to control form visibility
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 6000); // Change image every 3 seconds
+    }, 6000); // Change image every 6 seconds
 
     return () => clearInterval(interval); // Cleanup interval on component unmount
   }, []);
@@ -31,6 +31,14 @@ const Hero = () => {
     );
   };
 
+  const handleOpenForm = () => {
+    setIsFormOpen(true); // Open the form
+  };
+
+  const handleCloseForm = () => {
+    setIsFormOpen(false); // Close the form
+  };
+
   return (
     <>
       <div className="hero" id="hero">
@@ -39,6 +47,11 @@ const Hero = () => {
           alt={`Hero Banner ${currentIndex + 1}`}
           className="hero-banner-image"
         />
+        <div className="hero-content">
+          <button onClick={handleOpenForm} className="hero-button">
+            Get Started
+          </button>
+        </div>
         <div className="prev-arrow" onClick={goToPrevSlide}>
           &#9664;
         </div>
@@ -46,6 +59,9 @@ const Hero = () => {
           &#9654;
         </div>
       </div>
+
+      {/* Render the EnquiryForm component when isFormOpen is true */}
+      <EnquiryForm isOpen={isFormOpen} onClose={handleCloseForm} />
     </>
   );
 };
